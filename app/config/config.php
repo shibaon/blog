baccf8fa5d1dbce973a858ed2b1d2a2964af36eb
Valery Shibanov <shibaon@gmail.com>
2015-06-21 22:36:25 +0700

    Adaptive images

--- app/config/config.php
+++ app/config/config.php
@@ -11,7 +11,7 @@
 		'bundles' => include(__DIR__.'/./bundles.php'),
 		'db' => $getParameter('db', null),
 		'twig' => $getParameter('twig', false),
-		'assetsVersion' => 'v29',
+		'assetsVersion' => 'v1',
 		'locale' => $getParameter('locale', 'ru'),
 		'settings' => [
 			'sitename' => [
