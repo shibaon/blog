2f6ac447c24a46fd8094f57d19a6b03ad24896b7
Valery Shibanov <shibaon@gmail.com>
2015-06-23 19:15:17 +0700

    Margin visual fix

--- app/config/config.php
+++ app/config/config.php
@@ -11,7 +11,7 @@
 		'bundles' => include(__DIR__.'/./bundles.php'),
 		'db' => $getParameter('db', null),
 		'twig' => $getParameter('twig', false),
-		'assetsVersion' => 'v3',
+		'assetsVersion' => 'v4',
 		'locale' => $getParameter('locale', 'ru'),
 		'settings' => [
 			'sitename' => [
